87bb9c12117932420359b25535a10b5e28e08924
Jorge Torrado <jorgeetorradoforero@gmail.com>
2024-09-04 10:48:44 -0500

    backtracking estético

--- multiMat.py
+++ multiMat.py
@@ -37,13 +37,31 @@ def mat_mult_BU(d, b, e, m):
     # Retornar la matriz de costos calculada
     return m[1][e-1]
 
-def backTrackingMat(m, l,d):
-    for i in range(1,l-1):
-        if(m[1][i]+m[i+1][l-1]+(d[0]*d[i]*d[l-1]) == m[1][l-1]):
-            pasosMultiMat.append(1)
-            pasosMultiMat.append(i)
-            pasosMultiMat.append(i+1)
-            pasosMultiMat.append(l-1)
+def backTrackingMat(m,b,e,d):
+    #caso base
+    if(b == e):
+        #esto es por estética, para agregar el * de la multiplicación 
+        if(pasosMultiMat[len(pasosMultiMat)-1] == ")"):
+            pasosMultiMat.append("*")
+            pasosMultiMat.append(str(b))
+        else:
+            pasosMultiMat.append(str(b))
+            pasosMultiMat.append("*")
+        
+    #se simula la k, para recorrer y ver en qué orden sale
+    for k in range(b,e):
+        if(m[b][k]+m[k+1][e]+(d[b-1]*d[k]*d[e]) == m[b][e]):
+            #se agrega un * entre )( para que se vea más estético
+            if(len(pasosMultiMat) > 0):
+                if(pasosMultiMat[len(pasosMultiMat)-1] == ")"):
+                    pasosMultiMat.append("*")
+            pasosMultiMat.append("(")
+            backTrackingMat(m,b,k,d)
+            backTrackingMat(m,k+1,e,d)
+            #if para corregir estética (que no quede un * y justo despues un cierre de paréntesis)
+            if(pasosMultiMat[len(pasosMultiMat)-1] == "*"):
+                pasosMultiMat.pop()
+            pasosMultiMat.append(")")
     return
 
 #función inicial que dio el profe, no la comento pq mucho texto
@@ -60,7 +78,7 @@ def mat_mult(A):
     cost = mat_mult_BU(A,0,n,m)
     end = time.perf_counter_ns()
     print(f'Tiempo total: {(end - init)}')
-    backTrackingMat(m,n, A)
+    backTrackingMat(m,1,n-1, A)
     return cost
 # end def
 
@@ -75,6 +93,9 @@ A = [int(a) for a in Af][0:]
 
 print('Diagonales de matrices leídas del archivo: \n',A,'\n')
 print('el costo mínimo es: ',mat_mult(A))
-print("pasando por las matrices: ")
-for i in range(0, len(pasosMultiMat)-1,2):
-    print(pasosMultiMat[i],' - ', pasosMultiMat[i+1])
+print("Multiplicando matrices en el siguiente orden: ")
+#quedan un paréntesis inicial y final como de relleno, entonces por estética me los bajo
+del pasosMultiMat[0]
+pasosMultiMat.pop()
+for simbolo in pasosMultiMat:
+    print(simbolo, end="")
